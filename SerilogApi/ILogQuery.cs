6e4c00e541ff2d970e99c928803930872c9a523e
adamfoneil <adamfoneil@proton.me>
2026-09-05 11:11:46 -0400

    comments

--- SerilogApi/ILogQuery.cs
+++ SerilogApi/ILogQuery.cs
@@ -18,12 +18,24 @@ public record LogEntry(
 
 public class LogCriteria
 {
+    /// <summary>
+    /// allows values like 
+    /// "30m" = within 30 minutes ago. m = minutes ago, h = hours ago, d = days ago
+    /// "3-4h" = between 3 and 4 hours ago
+    /// "3:15am" = a specific time plus/minus 5 minutes (default before/after margin)
+    /// "4:27pm*3" = a specific time plus/minus 3 minute margin before/after
+    /// </summary>
     public string? DateTimeExpression { get; set; }
     public string? Level { get; set; }
-    public string? SourceContext { get; set; }
     public string? MessageContains { get; set; }
     public string? MessageExcludes { get; set; }
+    /// <summary>
+    /// used when the message payload itself is json, and we're inspecting parts of the message
+    /// </summary>
     public Dictionary<string, string> MessageProperties { get; set; } = [];
+    /// <summary>
+    /// typical structured log properties
+    /// </summary>
     public Dictionary<string, string> Properties { get; set; } = [];
     /// <summary>
     /// zero = no limit
