6600cac4b4a34a4973b29770f1b7eb16395f6ea4
adamfoneil <adamfoneil@proton.me>
2026-09-07 08:12:58 -0400

    started on query API

---
+++ QueryApi.MySql/ColumnConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace QueryApi.MySql;
+
+public enum LogTableColumns
+{
+    Timestamp,
+    MessageTemplate,
+    Message,
+    PropertiesJson
+}
+
+public class ColumnConfiguration(
+    string serilogTableName,
+    IDictionary<LogTableColumns, string> columnMappings)
+{
+    public string TableName { get; } = serilogTableName;
+
+    public Dictionary<LogTableColumns, string> ColumnMappings { get; } = columnMappings.ToDictionary();
+}
---
+++ QueryApi.MySql/MySqlLogQuery.cs
@@ -0,0 +1,31 @@
+﻿using SerilogApi;
+
+namespace QueryApi.MySql;
+
+public class MySqlLogQuery(string connectionString, ColumnConfiguration columnConfig) : ILogQuery
+{
+    private readonly string _connectionString = connectionString;
+    private readonly ColumnConfiguration _columnConfig = columnConfig;
+
+    private string BuildQuery((LogTableColumns Column, string Expression)[] concatExpressions) => $"SELECT {ColumnNames(concatExpressions)} FROM `{_columnConfig.TableName}`";
+
+    private string ColumnNames((LogTableColumns Column, string Expression)[] concatExpressions) => 
+        string.Join(", ", _columnConfig.ColumnMappings.Select(col => $"`{col.Value}`").Concat(concatExpressions.Select(expr => ExtractPropertyExpression(expr.Column, expr.Expression))));
+
+    private string ExtractPropertyExpression(LogTableColumns sourceColumn, string propertyExpression) => $"`{_columnConfig.ColumnMappings[sourceColumn]}`->>'{propertyExpression}'";
+
+    public Task<LogEntry[]> QueryAsync(LogCriteria filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ErrorInfo[]> RecentErrorsAsync(string? dateTimeExpression = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LogEntry[]> TraceAsync(string requestId)
+    {
+        throw new NotImplementedException();
+    }
+}
